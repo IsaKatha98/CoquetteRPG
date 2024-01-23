0cd12cc9b795e824935b3d5e286f2ae9d412d691
15Carmen <carmen.martin@iesnervion.es>
2024-01-23 12:48:41 +0100

    animaciones de movimiento hechas

--- Assets/Scripts/BunnyMovement.cs
+++ Assets/Scripts/BunnyMovement.cs
@@ -8,14 +8,35 @@ public class BunnyMovement : MonoBehaviour
     private float Vertical;
 
     public float Speed;
+    public Animator animator;
 
-    private void Update()
+    private void FixedUpdate()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
         Vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3 (Horizontal, Vertical);
 
+        AnimateMovement(direction);
+
         transform.position += direction * Speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
